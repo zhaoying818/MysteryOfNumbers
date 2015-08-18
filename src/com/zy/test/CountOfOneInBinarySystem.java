80875e93276799dcd24f3e68d620a8e03ed5d5cd
ZhaoYing <zhaoying1258@qq.com>
2015-08-18 19:16:35 +0800

    CountOfOneInBinarySystem3.0 使用n&(n-1)是否为零来判断1的个数

--- src/com/zy/test/CountOfOneInBinarySystem.java
+++ src/com/zy/test/CountOfOneInBinarySystem.java
@@ -18,17 +18,27 @@ public class CountOfOneInBinarySystem {
 //	}
 	
 	
-	/**
-	 * 移位之后，使用位操作判断是否存在1
-	 */
+//	/**
+//	 * 移位之后，使用位操作判断是否存在1
+//	 */
+//	public static int countOfOneInBinS(int n){
+//		int num = 0;
+//		while(n>0){
+//			num += n &0x01;
+//			n >>= 1;
+//		}
+//		return num;
+//	}
+	
 	public static int countOfOneInBinS(int n){
-		int num = 0;
+		int num =0;
 		while(n>0){
-			num += n &0x01;
-			n >>= 1;
+			n &= (n-1);
+			num++;
 		}
 		return num;
 	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("3中1的个数为:" + countOfOneInBinS(3));
