a8b5d7d03c03158048fcd5c783c2878dce60d0c9
ZhaoYing <zhaoying1258@qq.com>
2015-08-22 11:58:13 +0800

    SearchMaxAndMin 上个版本的改进，不改变原数组（先比较两个相邻数的大小，然后大的与最大值比较，小的与最小值比较）

--- src/com/zy/test/SearchMaxAndMin.java
+++ src/com/zy/test/SearchMaxAndMin.java
@@ -1,45 +1,44 @@
 package com.zy.test;
-/** 
- * @author ZhaoYing E-mail:zhaoying1258@qq.com 
- * @version Creation-time：2015年8月22日 上午10:25:08 
+
+/**
+ * @author ZhaoYing E-mail:zhaoying1258@qq.com
+ * @version Creation-time：2015年8月22日 上午10:25:08
  */
 public class SearchMaxAndMin {
-	
-	public static int[] searchMaxAndMin(int[] a){
+
+	public static int[] searchMaxAndMin(int[] a) {
 		int MAX = Integer.MIN_VALUE;
 		int MIN = Integer.MAX_VALUE;
-		
-		for(int i=0;i<a.length-1;i+=2){
-			if(a[i]<a[i+1]){
-				int temp = a[i];
-				a[i] = a[i+1];
-				a[i+1] = temp;
+
+		for (int i = 0; i < a.length - 1; i += 2) {
+			if (a[i] < a[i + 1]) {
+				if (a[i + 1] > MAX)
+					MAX = a[i + 1];
+				if (a[i] < MIN)
+					MIN = a[i];
+			} else {
+				if (a[i] > MAX)
+					MAX = a[i];
+				if (a[i + 1] < MIN)
+					MIN = a[i + 1];
 			}
 		}
-		for(int i=0;i<a.length;i+=2){
-			if(MAX<a[i])
-				MAX = a[i];
-		}
-		for(int i=1;i<a.length;i+=2){
-			if(MIN>a[i])
-				MIN = a[i];
-		}
+			
 		if(a.length%2!=0){
+			if(MAX<a[a.length-1])
+				MAX = a[a.length-1];
 			if(MIN>a[a.length-1])
 				MIN = a[a.length-1];
 		}
 
-		int [] ret = new int[]{MAX,MIN};
+		int[] ret = new int[] { MAX, MIN };
 		return ret;
 	}
-		
-		
-		
-		
+
 	public static void main(String[] args) {
-		int [] a = {5,6,8,7,3,9,2};
-		int [] ret = searchMaxAndMin(a);
-		
+		int[] a = { 5, 6, 8, 7, 3, 9, 2 };
+		int[] ret = searchMaxAndMin(a);
+
 		System.out.println("MAX of Array: " + ret[0]);
 		System.out.println("MIN of Array: " + ret[1]);
 	}
