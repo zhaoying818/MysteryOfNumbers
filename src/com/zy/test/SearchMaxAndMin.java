2ea0345f5ce2c6d3fcd3e9e30e29e20226bf58a5
ZhaoYing <zhaoying1258@qq.com>
2015-08-22 11:39:46 +0800

    SearchMaxAndMin 分组后进行比较（偶数位大于奇数位）

--- src/com/zy/test/SearchMaxAndMin.java
+++ src/com/zy/test/SearchMaxAndMin.java
@@ -6,27 +6,42 @@ package com.zy.test;
 public class SearchMaxAndMin {
 	
 	public static int[] searchMaxAndMin(int[] a){
-		
 		int MAX = Integer.MIN_VALUE;
 		int MIN = Integer.MAX_VALUE;
 		
-		for(int i=0;i<a.length;i++){
+		for(int i=0;i<a.length-1;i+=2){
+			if(a[i]<a[i+1]){
+				int temp = a[i];
+				a[i] = a[i+1];
+				a[i+1] = temp;
+			}
+		}
+		for(int i=0;i<a.length;i+=2){
 			if(MAX<a[i])
 				MAX = a[i];
+		}
+		for(int i=1;i<a.length;i+=2){
 			if(MIN>a[i])
 				MIN = a[i];
 		}
-		int[] ret = new int[]{MAX,MIN};
+		if(a.length%2!=0){
+			if(MIN>a[a.length-1])
+				MIN = a[a.length-1];
+		}
+
+		int [] ret = new int[]{MAX,MIN};
 		return ret;
 	}
-
+		
+		
+		
+		
 	public static void main(String[] args) {
-		int [] a = {5,6,8,3,7,9,2};
+		int [] a = {5,6,8,7,3,9,2};
 		int [] ret = searchMaxAndMin(a);
+		
 		System.out.println("MAX of Array: " + ret[0]);
 		System.out.println("MIN of Array: " + ret[1]);
-
-
 	}
 
 }
