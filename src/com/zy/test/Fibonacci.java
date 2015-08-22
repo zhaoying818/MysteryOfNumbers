db2487c07b448cecc54388bfc597d9d35ddc6271
ZhaoYing <zhaoying1258@qq.com>
2015-08-22 10:13:17 +0800

    Fibonacci 递归方法求解Fibonacci数列

--- src/com/zy/test/Fibonacci.java
+++ src/com/zy/test/Fibonacci.java
@@ -1,20 +1,22 @@
 package com.zy.test;
-/** 
- * @author ZhaoYing E-mail:zhaoying1258@qq.com 
- * @version Creation-time：2015年8月22日 上午9:59:58 
+
+/**
+ * @author ZhaoYing E-mail:zhaoying1258@qq.com
+ * @version Creation-time：2015年8月22日 上午9:59:58
  */
 public class Fibonacci {
-	
+
 	/**
 	 * 递归方法
 	 */
-	public static int fibonacci(int n){
-		if(n<=0)
+	public static int fibonacci(int n) {
+		if (n <= 0)
 			return 0;
-		else if(n==1)
+		else if (n == 1)
 			return 1;
-		else
-			return fibonacci(n-1) + fibonacci(n-1);
+		else {
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
 	}
 
 	public static void main(String[] args) {
