b55097addd416a11951330b40264c23845172f4b
ZhaoYing <zhaoying1258@qq.com>
2015-09-01 21:58:28 +0800

    RightShiftKBit 数组循环右移，考虑移位的大小

--- src/com/zy/test/RightShiftKBit.java
+++ src/com/zy/test/RightShiftKBit.java
@@ -11,6 +11,7 @@ public class RightShiftKBit {
 	 */
 	public static int[] getRightShiftK(int[] a, int k){
 		int len = a.length;
+		k %= len;
 		while((k--)>0){
 			int t= a[len-1];
 			for(int i = len-1;i>0;i--)
